1888033ac4bc959b4b77952f56244cbb14bfeccf
salleyfang <skfang@uw.edu>
2025-02-21 16:41:11 -0800

    data loaded

--- main.js
+++ main.js
@@ -24,7 +24,8 @@ const svg2_RENAME = d3.select("#lineChart2")
 // 2.a: LOAD...
 Promise.all([
     d3.csv("CLEANED-grouped-airport.csv"),
-    d3.csv("CLEANED-make.csv")
+    d3.csv("CLEANED-grouped-make.csv")
+    // d3.csv("CLEANED-make.csv")
 ]).then(([airportData, makeData]) => {
     // airport transformation
     airportData.forEach(d => {
@@ -33,30 +34,30 @@ Promise.all([
     });
     console.log(airportData);
     // make transformation
-    makeData.forEach(d => {
-        // group Injury_Severity to make counting easier
+    // makeData.forEach(d => {
+    //     // group Injury_Severity to make counting easier
 
-        // keep Make, Injury_Severity columns ONLY
-        delete d.Accident_Number;
-        delete d.Event_Date;
-        delete d.Accident_Year;
-        delete d.Location;
-        delete d.Country;
-        delete d.Latitude;
-        delete d.Longitude;
-        delete d.Airport_Code;
-        delete d.Airport_Name;
-        delete d.Aircraft_Damage;
-        delete d.Registration_Number;
-        delete d.Model;
-        delete d.Schedule;
-        delete d.Air_Carrier;
-        delete d.Total_Fatal_Injuries;
-        delete d.Total_Serious_Injuries;
-        delete d.Total_Uninjured;
-        delete d.Weather_Condition;
-        delete d.Broad_Phase_of_Flight;
-    });
+    //     // keep Make, Injury_Severity columns ONLY
+    //     delete d.Accident_Number;
+    //     delete d.Event_Date;
+    //     delete d.Accident_Year;
+    //     delete d.Location;
+    //     delete d.Country;
+    //     delete d.Latitude;
+    //     delete d.Longitude;
+    //     delete d.Airport_Code;
+    //     delete d.Airport_Name;
+    //     delete d.Aircraft_Damage;
+    //     delete d.Registration_Number;
+    //     delete d.Model;
+    //     delete d.Schedule;
+    //     delete d.Air_Carrier;
+    //     delete d.Total_Fatal_Injuries;
+    //     delete d.Total_Serious_Injuries;
+    //     delete d.Total_Uninjured;
+    //     delete d.Weather_Condition;
+    //     delete d.Broad_Phase_of_Flight;
+    // });
     console.log(makeData);
     // 2.b: ... AND TRANSFORM DATA
 
